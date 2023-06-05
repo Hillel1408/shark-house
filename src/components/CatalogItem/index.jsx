168b55bdbbb56b2628696716ffc8d461f8fc2d9c
Hillel1408 <etemax@bk.ru>
2023-06-05 19:24:52 +0300

    shark-house

---
+++ src/components/CatalogItem/index.jsx
@@ -0,0 +1,50 @@
+import styles from './CatalogItem.module.scss';
+
+function CatalogItem() {
+    return (
+        <div className={styles.catalogItem}>
+            <h3 className={styles.catalogItemTitle}>
+                Eames Lounge Chair & Ottoman
+            </h3>
+            <img src="/img-1.jpg" alt="" />
+            <div className={styles.catalogItemImages}>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+                <div className={styles.catalogItemImage}>
+                    <img src="/img-2.jpg" alt="" />
+                </div>
+            </div>
+            <div className={styles.catalogItemFlex}>
+                <span className={styles.catalogItemTitle}>75,000 руб.</span>
+                <button className={styles.catalogItemBtn}>Купить</button>
+            </div>
+        </div>
+    );
+}
+
+export { CatalogItem };
--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -1,56 +1,13 @@
 import classNames from 'classnames';
+import { CatalogItem } from '../../components/CatalogItem';
 import styles from './Catalog.module.scss';
 
 function Catalog() {
     return (
         <div className={classNames('_container', styles.catalog)}>
             <div className={styles.catalogItems}>
-                <div className={styles.catalogItem}>
-                    <h3 className={styles.catalogItemTitle}>
-                        Eames Lounge Chair & Ottoman
-                    </h3>
-                    <img src="/img-1.jpg" alt="" />
-                    <div className={styles.catalogItemImages}>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                        <div className={styles.catalogItemImage}>
-                            <img src="/img-2.jpg" alt="" />
-                        </div>
-                    </div>
-                    <div className={styles.catalogItemFlex}>
-                        <span className={styles.catalogItemTitle}>
-                            75,000 руб.
-                        </span>
-                        <button className={styles.catalogItemBtn}>
-                            Купить
-                        </button>
-                    </div>
-                </div>
+                <CatalogItem /> <CatalogItem /> <CatalogItem /> <CatalogItem />
+                <CatalogItem />
             </div>
         </div>
     );
