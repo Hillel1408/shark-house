3f3986904c0768d30618f880165e17275c99c831
Hillel1408 <etemax@bk.ru>
2023-06-05 14:10:42 +0300

    shark-house

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Header } from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/globals.scss';
 
 function App() {
     return (
-        <>
+        <Router basename="shark-house">
             <Header />
-        </>
+        </Router>
     );
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,52 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 function Header() {
-    return <header className="_container"></header>;
+    return (
+        <header className={styles.header}>
+            <div className={classNames('_container', styles.headerContainer)}>
+                <NavLink to="/">
+                    <img src="/logo.png" alt="" />
+                </NavLink>
+                <div className={styles.headerFlex}>
+                    <div className={styles.headerSocialMedia}>
+                        <ul>
+                            <li>
+                                <a href="/">
+                                    <img src="/icon-1.svg" alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    <img src="/icon-2.svg" alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    <img src="/icon-3.svg" alt="" />
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="tel:+7(985)950-85-25">+7(985)950-85-25</a>
+                    </div>
+                    <nav className={styles.headerNav}>
+                        <ul>
+                            <li>
+                                <NavLink to="/">О нас</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/">Доставка и гарантия</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/">Каталог</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export { Header };
