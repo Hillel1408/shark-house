b6acd99effa8b9370f1ed9b5409ab7524dfec432
Hillel1408 <etemax@bk.ru>
2023-06-07 10:03:05 +0300

    shark-house

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,20 @@
+import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
 import styles from './Footer.module.scss';
 
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <div className="_container">
+            <div className={classNames('_container', styles.footerFlex)}>
                 <NavLink to="/">
                     <img
                         className={styles.footerLogo}
                         src="/shark-house/logo.png"
                         alt=""
                     />
+                </NavLink>
+                <NavLink to="/admin" className={styles.footerLink}>
+                    Вход
                 </NavLink>
             </div>
         </footer>
