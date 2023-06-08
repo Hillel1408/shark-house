46abc1dc284b096a9e1f4ef7d74acef4d3c5bfe3
Hillel1408 <etemax@bk.ru>
2023-06-08 12:10:37 +0300

    shark-house

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Header } from './components/Header';
 import { AboutUs } from './pages/AboutUs';
 import { Delivery } from './pages/Delivery';
 import { Catalog } from './pages/Catalog';
-import { Order } from './pages/ Order';
+import { Order } from './pages/Order';
 import { Footer } from './components/Footer';
 import { Admin } from './pages/Admin';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
